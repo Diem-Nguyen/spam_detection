6a3bf97c1ea300edea5db216456d60afde9e7f17
Diem Nguyen <diem@skenariolabs.com>
2019-09-29 11:46:24 +0300

    this is a test

---
+++ spam_detection.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(wordcloud)
+library(tm)
+
+# load data
+raw_df <- sms_spam <- read_csv("data/sms_spam.csv")
+
+# convert type to factor
+raw_df$type <- raw_df$type %>% as.factor
+
+# word cloud for spam
+
+par(bg = "black") 
+set.seed(1709)
+
+
+wordcloud(raw_df  %>% filter(type == "spam") %>% pull(text), 
+          max.words = 200, 
+          random.order = FALSE, 
+          rot.per = 0.35, 
+          font = 2,
+          colors = brewer.pal(8, "Dark2"))
+
+
+
+# prepare data for modeling
+
+sms_corpus <- sms_raw$text %>% 
+  VectorSource() %>% 
+  VCorpus() %>% 
+  tm_map(content_transformer(tolower)) %>% 
+  tm_map(removeNumbers) %>% 
+  tm_map(removeWords, stopwords()) %>% 
+  tm_map(removePunctuation) %>% 
+  tm_map(stripWhitespace)
+
+
+
+## this is a test
