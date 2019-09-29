cd3c21b52db80bc56cada77c729fa9be9d345eeb
Diem Nguyen <diem@skenariolabs.com>
2019-09-29 12:33:57 +0300

    visualization of frequent texts in spam message

--- spam_detection.R
+++ spam_detection.R
@@ -13,19 +13,21 @@ raw_df$type <- raw_df$type %>% as.factor
 par(bg = "black") 
 set.seed(1709)
 
-
-wordcloud(raw_df  %>% filter(type == "spam") %>% pull(text), 
-          max.words = 200, 
+word <- wordcloud(raw_df  %>% filter(type == "spam") %>% pull(text), 
+          max.words = 150, 
           random.order = FALSE, 
           rot.per = 0.35, 
           font = 2,
           colors = brewer.pal(8, "Dark2"))
 
-
+# save as image
+png("word.png")
+print(word)     
+dev.off() 
 
 # prepare data for modeling
 
-sms_corpus <- sms_raw$text %>% 
+sms_corpus <- raw_df$text %>% 
   VectorSource() %>% 
   VCorpus() %>% 
   tm_map(content_transformer(tolower)) %>% 
@@ -34,6 +36,5 @@ sms_corpus <- sms_raw$text %>%
   tm_map(removePunctuation) %>% 
   tm_map(stripWhitespace)
 
+# convert to sparse matrix 
 
-
-## this is a test
